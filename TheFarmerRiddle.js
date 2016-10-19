1ebcf0e021f8fa009d0f9ff5d1a867295960529e
webdev01 <gabemeyerson@gmail.com>
2016-10-19 15:09:46 -0400

    Intial Commit

---
+++ TheFarmerRiddle.js
@@ -0,0 +1,18 @@
+/**
+ * @author Meyerson, Gabe (gabemeyerson@gmail.com)
+ * @version 0.0.1
+ * @summary The famer, the goat, the wolf, and the cabbage riddle || created 10.19.16
+ * @todo
+ */
+
+"use strict";
+const PROMPT = require('readline-sync');
+
+let
+const 
+
+function main(){
+    
+}
+
+main();
